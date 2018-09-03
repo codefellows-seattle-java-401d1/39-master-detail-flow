065ea5bf6d0948d1ae3ba869d6f2055d6afe879c
Amy Cohen <anbcohen@gmail.com>
2018-09-03 10:20:09 -0700

    added MapsFragment file and ErrandListFragment file and began conversion on the ErrandListFragment. My IntelliJ would not allow me to refactor the Activity to a Fragment so I'm doing it in a longer way than Steve, but I seem to be able to follow along okay

---
+++ app/src/main/java/com/amycohen/lab39masterdetailflow/MapsFragment.java
@@ -0,0 +1,13 @@
+package com.amycohen.lab39masterdetailflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapsFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps_fragment);
+    }
+}
