4c46311e59553c055896b19a3152a1acbd8a54e7
Amy Cohen <anbcohen@gmail.com>
2018-09-03 12:08:24 -0700

    refactored the Maps and ErrandList Activities to extend the SingleFragmentActivity

--- app/src/main/java/com/amycohen/lab39masterdetailflow/ErrandListActivity.java
+++ app/src/main/java/com/amycohen/lab39masterdetailflow/ErrandListActivity.java
@@ -1,21 +1,11 @@
 package com.amycohen.lab39masterdetailflow;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class ErrandListActivity extends AppCompatActivity {
+public class ErrandListActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_fragment);
-
-        Fragment fragment = new ErrandListFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction()
-                .add(R.id.single_fragment_container, fragment)
-                .commit();
+    Fragment getFragment() {
+        return new ErrandListFragment();
     }
 }
--- app/src/main/java/com/amycohen/lab39masterdetailflow/MapsActivity.java
+++ app/src/main/java/com/amycohen/lab39masterdetailflow/MapsActivity.java
@@ -1,22 +1,11 @@
 package com.amycohen.lab39masterdetailflow;
 
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 
-public class MapsActivity extends AppCompatActivity  {
+public class MapsActivity extends SingleFragmentActivity  {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_fragment);
-
-        Fragment fragment = new MapsFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction()
-                .add(R.id.single_fragment_container, fragment)
-                .commit();
+    Fragment getFragment() {
+        return new MapsFragment();
     }
-
 }
