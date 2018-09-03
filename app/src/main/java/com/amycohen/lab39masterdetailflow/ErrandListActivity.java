148810d27aabd828fe4fcad55c4c00fa9c5c977f
Amy Cohen <anbcohen@gmail.com>
2018-09-03 10:27:56 -0700

    added the single_fragment layout to contain the container for the fragment to pop into. refactored the ErrandListActivity to use a fragment

--- app/src/main/java/com/amycohen/lab39masterdetailflow/ErrandListActivity.java
+++ app/src/main/java/com/amycohen/lab39masterdetailflow/ErrandListActivity.java
@@ -1,5 +1,7 @@
 package com.amycohen.lab39masterdetailflow;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,7 +19,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ErrandListActivity extends AppCompatActivity implements ValueEventListener {
+public class ErrandListActivity extends AppCompatActivity {
 
     @BindView(R.id.errands) RecyclerView recyclerView;
     LinearLayoutManager linearLayoutManager;
@@ -26,32 +28,38 @@ public class ErrandListActivity extends AppCompatActivity implements ValueEventL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_errand_list);
+        setContentView(R.layout.activity_single_fragment);
 
-        ButterKnife.bind(this);
+        Fragment fragment = new ErrandListFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .add(R.id.single_fragment_container, fragment)
+                .commit();
 
-        DatabaseReference errands = FirebaseDatabase.getInstance().getReference("errands");
-        errands.addValueEventListener(this);
-
-        linearLayoutManager = new LinearLayoutManager(this);
-        errandAdapter = new ErrandAdapter();
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(errandAdapter);
+//        ButterKnife.bind(this);
+//
+//        DatabaseReference errands = FirebaseDatabase.getInstance().getReference("errands");
+//        errands.addValueEventListener(this);
+//
+//        linearLayoutManager = new LinearLayoutManager(this);
+//        errandAdapter = new ErrandAdapter();
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//        recyclerView.setAdapter(errandAdapter);
     }
 
-    @Override
-    public void onDataChange(DataSnapshot dataSnapshot) {
-        List<Errand> errands = new ArrayList<>();
-
-        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-            errands.add(Errand.fromSnapshot(snapshot));
-        }
-        errandAdapter.errands = errands;
-        errandAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
-
-    }
+//    @Override
+//    public void onDataChange(DataSnapshot dataSnapshot) {
+//        List<Errand> errands = new ArrayList<>();
+//
+//        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//            errands.add(Errand.fromSnapshot(snapshot));
+//        }
+//        errandAdapter.errands = errands;
+//        errandAdapter.notifyDataSetChanged();
+//    }
+//
+//    @Override
+//    public void onCancelled(DatabaseError databaseError) {
+//
+//    }
 }
